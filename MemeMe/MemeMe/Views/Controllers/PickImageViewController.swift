4b4eb4662580a7c84327bd5b6e91a84179ee9840
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-09-20 20:24:04 +0800

    Added image picker

---
+++ MemeMe/MemeMe/Views/Controllers/PickImageViewController.swift
@@ -0,0 +1,42 @@
+//
+//  PickImageViewController.swift
+//  MemeMe
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 20/09/17.
+//  Copyright © 2017 Leonardo Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class PickImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Actions
+    
+    
+    @IBAction func pickImageClicked(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
+// MARK: - Image Picker Delegate
+
+extension PickImageViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
